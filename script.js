83639f0ae6f5d30a645c74ccc9b7f77163b88af6
Ryan Zielke <NeoAlchemy@gmail.com>
2024-12-31 11:03:52 -0600

    show geocode errors
    show geocode errors

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
+const map = new google.maps.Map(document.getElementById("map"), {
+  center: { lat: 32.9508297, lng: -97.2816533 },
+  zoom: 10,
+});
 
-function initMap(addresses) {
-  const bounds = new google.maps.LatLngBounds();
-  const markersArray = [];
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 32.9508297, lng: -97.2816533 },
-    zoom: 10,
-  });
+function setAddress(addresses) {
+  
   // initialize services
   const geocoder = new google.maps.Geocoder();
   const service = new google.maps.DistanceMatrixService();
@@ -15,13 +14,13 @@ function initMap(addresses) {
     console.log("address: " + address)
     geocoder.geocode({ address }, (results, status) => {
       if (status === "OK") {
-        console.log("address")
         const marker = new google.maps.Marker({
           map,
           position: results[0].geometry.location,
         });
       } else {
         console.error(`Geocode was not successful for the following reason: ${status}`);
+        document.getElementById("geocodeErrors").innerHTML = document.getElementById("geocodeErrors").innerHTML + address + "<br/>"
       }
     });
   });
@@ -29,33 +28,14 @@ function initMap(addresses) {
   drawCircle();
 }
   
-function deleteMarkers(markersArray) {
-  for (let i = 0; i < markersArray.length; i++) {
-    markersArray[i].setMap(null);
-  }
-
-  markersArray = [];
-}
-
-document.getElementById("go").onclick = function(){
-    window.initMap();
-}
-
-document.getElementById("dest"),onkeypress = function(e) {
-  if (e.keyCode == 13) {
-      window.initMap();
-  }
-}
-
 document.getElementById('fileInput').addEventListener('change', function(event) {
   const file = event.target.files[0];
   if (file) {
       const reader = new FileReader();
       reader.onload = function(e) {
           const content = e.target.result;
-          console.log(content);
           let rows = content.split(/\n/)
-          window.initMap(rows)
+          window.setAddress(rows)
       };
       reader.readAsText(file);
   }
